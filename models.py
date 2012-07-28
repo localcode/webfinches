1481412c9cd41272e7422741a523a70eb0e117e4
Benjamin Golder <benjamin.j.golder@gmail.com>
2012-07-28 10:21:20 -0700

    added TextItem model, for storing miscellaneous bits of content

--- models.py
+++ models.py
@@ -36,6 +36,30 @@ class Lookup(Named):
     class Meta:
         abstract=True
 
+class TextItem(models.Model):
+    """A very basic model for storing bits of text-based content"""
+    name = models.CharField(max_length=200)
+    content = models.TextField()
+    def __unicode__(self):
+        return "TextItem: %s" % self.name
+
+def textspace(namespace):
+    '''Uses the beginning of name strings retrieve all the
+    TextItem objects that share the same root in their name. Returns a dictionary
+    with the form:
+        {
+        'text_item_name':'text_item_content',
+        }
+    Can be used to quickly add text items to a context and then use them
+    directly in templates.
+    '''
+    objs = TextItem.objects.filter(name__startswith=namespace)
+    d = {}
+    for t in objs:
+            d[t.name] = t.content
+    return d
+
+
 class DataFile(Dated):
     """Data files represent individual file uploads.
     They are used to construct DataLayers.
@@ -45,6 +69,8 @@ class DataFile(Dated):
     upload = models.ForeignKey('UploadEvent', null=True, blank=True)
     def get_upload_path(self, filename):
         return 'uploads/%s/%s' % (self.upload.user.username, filename)
+    def __unicode__(self):
+        return "DataFile: %s" % self.name
 
 class DataLayer(Named, Authored, Dated, Noted):
     geometry_type = models.CharField(max_length=50, null=True, blank=True)
@@ -56,6 +82,8 @@ class DataLayer(Named, Authored, Dated, Noted):
 class UploadEvent(models.Model):
     user = models.ForeignKey(User)
     date = models.DateTimeField(auto_now_add=True)
+    def __unicode__(self):
+        return "UploadEvent: %s" % self.date
 
 class Tag(Lookup, Dated, Noted):
     layers = models.ManyToManyField(DataLayer)
